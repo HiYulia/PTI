86f7919f838b3538aec49f54daab6ceff155a466
HiYulia <32212658+HiYulia@users.noreply.github.com>
2023-07-24 11:33:40 +0800

    Update hyperparameters.py

--- configs/hyperparameters.py
+++ configs/hyperparameters.py
@@ -13,10 +13,10 @@ regulizer_alpha = 30
 
 ## Loss
 pt_l2_lambda = 1
-pt_lpips_lambda = 1
+pt_lpips_lambda = 5
 
 ## Steps
-LPIPS_value_threshold = 0.01
+LPIPS_value_threshold = 0.06
 max_pti_steps = 350
 first_inv_steps = 450
 max_images_to_invert = 30
